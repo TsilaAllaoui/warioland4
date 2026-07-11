3c232d89085bc57bef26a8cc9bbc31269cf6be76
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-11 19:41:15 +0000

    Match disasm_ball_lightning 100%

--- include/sprite_ai/ball_lightning.h
+++ include/sprite_ai/ball_lightning.h
@@ -1,8 +1,19 @@
 #ifndef SPRITE_AI_BALL_LIGHTNING_H
 #define SPRITE_AI_BALL_LIGHTNING_H
 
+#include "oam.h"
+#include "types.h"
+
+extern const struct AnimationFrame sBallLightningBoltFallingLeftOam[];
+extern const struct AnimationFrame sBallLightningBoltFallingRightOam[];
+extern const struct AnimationFrame sBallLightningBoltIdleOam[];
+extern const struct AnimationFrame sBallLightningTrailOam[];
+extern const struct AnimationFrame sBallLightningBoltDisappearingOam[];
+extern const struct AnimationFrame sBallLightningSpawnerOam[];
+extern const s16 sBallLightningTrailYVelocity[];
+
 void SpriteBallLightning(void);
-void SpriteUnknown9F(void);
-void SpriteUnknownA0(void);
+void SpriteBallLightningBolt(void);
+void SpriteBallLightningTrail(void);
 
 #endif  // SPRITE_AI_BALL_LIGHTNING_H
