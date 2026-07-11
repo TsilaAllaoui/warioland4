d382537d9c8daf4e71ec311d8c4fe2fb5c770b4e
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-11 14:36:17 +0000

    Match disasm_switch 100%

--- include/sprite_ai/switch.h
+++ include/sprite_ai/switch.h
@@ -1,7 +1,19 @@
 #ifndef SPRITE_AI_SWITCH_H
 #define SPRITE_AI_SWITCH_H
 
+#include "sprite.h"
+
+extern const struct AnimationFrame sSwitchIdleOam[];
+extern const struct AnimationFrame sEscapeSwitchPressedOam[];
+extern const struct AnimationFrame sEscapeSwitchActiveOam[];
+extern const struct AnimationFrame sSwitchReleasedOam[];
+extern const struct AnimationFrame sSwitchPressedOam[];
+extern const struct AnimationFrame sSwitchEffectIdleOam[];
+extern const struct AnimationFrame sEscapeSwitchWaitingOam[];
+extern const struct AnimationFrame sEscapeSwitchIdleOam[];
+extern const struct AnimationFrame sSwitchEffectActiveOam[];
+
 void SpriteSwitch(void);
-void SpriteUnknownAA(void);
+void SpriteSwitchEffect(void);
 
 #endif  // SPRITE_AI_SWITCH_H
