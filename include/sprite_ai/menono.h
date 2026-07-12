deca32ab06773626e75f3872776596d310b31c36
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-12 16:06:13 +0000

    Match disasm_menono 100%

--- include/sprite_ai/menono.h
+++ include/sprite_ai/menono.h
@@ -1,6 +1,21 @@
 #ifndef SPRITE_AI_MENONO_H
 #define SPRITE_AI_MENONO_H
 
+#include "oam.h"
+
+extern const struct AnimationFrame sMenonoPushedOam[];
+extern const struct AnimationFrame sMenonoTurningOam[];
+extern const struct AnimationFrame sMenonoPreparingAttackOam[];
+extern const struct AnimationFrame sMenonoWalkingOam[];
+extern const struct AnimationFrame sMenonoAttackingOam[];
+extern const struct AnimationFrame sMenonoStunnedOam[];
+extern const struct AnimationFrame sMenonoFallingOam[];
+extern const struct AnimationFrame sMenonoTackledOam[];
+extern const struct AnimationFrame sMenonoCrushedOam[];
+extern const struct AnimationFrame sMenonoGettingUpOam[];
+extern const struct AnimationFrame sMenonoTurningAroundOam[];
+extern const struct AnimationFrame sMenonoCrushedWhileStunnedOam[];
+
 void SpriteMenono(void);
 
 #endif  // SPRITE_AI_MENONO_H
