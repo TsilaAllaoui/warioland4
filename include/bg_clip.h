c1c9e28cf6129673011d06b24570f5dd1c03a0aa
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-13 19:04:07 +0000

    Match disasm_rotating_platform 100%

--- include/bg_clip.h
+++ include/bg_clip.h
@@ -4,5 +4,6 @@
 #include "gba.h"
 
 s32 func_806DAC0(u16, u16);
+s32 func_806DD44(u16 y, s32 x);
 
 #endif  // BG_CLIP_H
--- include/sprite_ai/rotating_platform.h
+++ include/sprite_ai/rotating_platform.h
@@ -1,6 +1,20 @@
 #ifndef SPRITE_AI_ROTATING_PLATFORM_H
 #define SPRITE_AI_ROTATING_PLATFORM_H
 
+#include "oam.h"
+#include "types.h"
+
+extern const s16 sUnk_83B31E8[];
+extern const s16 sUnk_83B326A[];
+extern const s16 sUnk_83B32EC[];
+extern const s16 sUnk_83B336E[];
+extern const s16 sUnk_83B33F0[];
+extern const s16 sUnk_83B3472[];
+extern const s16 sUnk_83B34F4[];
+extern const s16 sUnk_83B3576[];
+extern const struct AnimationFrame sUnk_83B4534[];
+extern const struct AnimationFrame sUnk_83B4544[];
+
 void SpriteRotatingPlatform(void);
 void SpriteUnknown94(void);
 void SpriteUnknown96(void);
