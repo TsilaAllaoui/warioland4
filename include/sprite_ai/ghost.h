47afa4d8f2dfbcaf4a601c1f936d330704a7c59c
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-13 17:03:34 +0000

    Match disasm_ghost 100%

--- include/sprite_ai/ghost.h
+++ include/sprite_ai/ghost.h
@@ -1,6 +1,55 @@
 #ifndef SPRITE_AI_GHOST_H
 #define SPRITE_AI_GHOST_H
 
+#include "sprite.h"
+
+struct GhostSecondarySpriteData {
+    u8 status;
+    u8 unk1;
+    u8 id;
+    u8 unk3;
+    u8 unk4;
+    u8 unk5;
+    u8 unk6;
+    u8 unk7;
+    u16 yPosition;
+    u16 xPosition;
+};
+
+extern u8 gUnk_3000964[];
+extern struct GhostSecondarySpriteData gSecondarySpriteData[];
+
+extern const s16 sUnk_83C5598[];
+extern const s16 sUnk_83C55EA[];
+extern const s16 sUnk_83C56E0[];
+extern const u16 sUnk_83C5644[];
+extern const u16 sUnk_83C56AE[];
+extern const struct AnimationFrame sUnk_83C53B4[];
+extern const struct AnimationFrame sUnk_83C53FC[];
+extern const struct AnimationFrame sUnk_83C5364[];
+extern const struct AnimationFrame sUnk_83C538C[];
+extern const struct AnimationFrame sUnk_83C53C4[];
+extern const struct AnimationFrame sUnk_83C5568[];
+extern const struct AnimationFrame sUnk_83C5528[];
+
+void func_803F214(void);
+void func_803F264(void);
+void func_803F2B4(void);
+u32 func_803F320(void);
+void func_803F414(void);
+void func_803F528(void);
+void func_803F66C(void);
+void func_803F718(void);
+void func_803F7CC(void);
+void func_803F944(void);
+void func_803F958(void);
+void func_803FB14(void);
+void func_803FB28(void);
+void func_803FC70(void);
+void func_803FCF8(void);
+void func_803FDEC(void);
+void func_803FE54(void);
+void func_803FEC8(void);
 void SpriteGhost(void);
 void SpriteUnknownA7(void);
 void SpriteUnknownA8(void);
