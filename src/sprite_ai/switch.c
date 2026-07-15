a53d30ab1d2faef9866d5058d71d1bf426316f68
TsilaAllaoui <tsila.allaoui@gmail.com>
2026-07-15 20:17:37 +0000

    Convert sprite data to normal explicit OAM arrays

--- src/sprite_ai/switch.c
+++ src/sprite_ai/switch.c
@@ -10,6 +10,520 @@
 #include "wario.h"
 #include "gba/m4a.h"
 
+#include "oam.h"
+
+/* Sprite data reconstructed from the original contiguous ROM region. */
+
+const u16 sSwitchIdleOam_Frame1[] = {
+    4,
+    OAM_ENTRY(-12, -47, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchPressedOam_Frame1[] = {
+    4,
+    OAM_ENTRY(-12, -45, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchPressedOam_Frame2[] = {
+    4,
+    OAM_ENTRY(-12, -40, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchPressedOam_Frame3[] = {
+    4,
+    OAM_ENTRY(-12, -41, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame9[] = {
+    5,
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-7, -21, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sEscapeSwitchIdleOam_Frame1[] = {
+    4,
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame1[] = {
+    4,
+    OAM_ENTRY(-12, -42, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -34, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -18, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame2[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -43, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -35, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -19, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame6[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -44, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -36, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -20, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame5[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -45, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -37, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -21, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame4[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -46, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -38, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -22, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame3[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -47, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -39, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -23, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame2[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -48, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -40, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -24, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame1[] = {
+    4,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame3[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -44, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -36, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -20, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -16, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame4[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -45, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -37, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -21, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -16, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame5[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -46, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -38, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -22, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -17, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame6[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -47, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -39, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -23, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -18, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame7[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -48, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -40, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -24, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -19, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchReleasedOam_Frame8[] = {
+    5,
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-7, -19, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchPressedOam_Frame1[] = {
+    6,
+    OAM_ENTRY(-6, -43, SPRITE_SIZE_16x8, 0, 503, 5, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-6, -21, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchPressedOam_Frame2[] = {
+    6,
+    OAM_ENTRY(-6, -43, SPRITE_SIZE_16x8, 0, 505, 5, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-7, -22, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchPressedOam_Frame3[] = {
+    6,
+    OAM_ENTRY(6, -22, SPRITE_SIZE_8x8, 0, 168, 4, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-8, -21, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchPressedOam_Frame4[] = {
+    6,
+    OAM_ENTRY(6, -22, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-7, -23, SPRITE_SIZE_16x16, 0, 438, 5, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame1[] = {
+    1,
+    OAM_ENTRY(6, -20, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame2[] = {
+    1,
+    OAM_ENTRY(6, -21, SPRITE_SIZE_8x8, 0, 168, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame3[] = {
+    4,
+    OAM_ENTRY(2, -26, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(1, -18, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(8, -23, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(8, -15, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame4[] = {
+    1,
+    OAM_ENTRY(12, -19, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame5[] = {
+    4,
+    OAM_ENTRY(8, -23, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(6, -13, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(17, -21, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+    OAM_ENTRY(14, -12, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame6[] = {
+    1,
+    OAM_ENTRY(8, -15, SPRITE_SIZE_8x8, 0, 167, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame7[] = {
+    1,
+    OAM_ENTRY(9, -15, SPRITE_SIZE_8x8, 0, 168, 4, 0),
+};
+
+const u16 sSwitchEffectIdleOam_Frame8[] = {
+    4,
+    OAM_ENTRY(9, -24, SPRITE_SIZE_8x8, ST_OAM_HFLIP, 167, 4, 0),
+    OAM_ENTRY(17, -14, SPRITE_SIZE_8x8, ST_OAM_HFLIP, 167, 4, 0),
+    OAM_ENTRY(1, -18, SPRITE_SIZE_8x8, ST_OAM_HFLIP, 167, 4, 0),
+    OAM_ENTRY(9, -8, SPRITE_SIZE_8x8, ST_OAM_HFLIP, 167, 4, 0),
+};
+
+const u8 sSwitchRawData_83B57CC[] = {
+    0x02, 0x00, 0xF8, 0x00, 0xF3, 0x41, 0x7C, 0x51, 0xF8, 0x00, 0xFC, 0x51, 0x7C, 0x51, 0x02, 0x00,
+    0xF8, 0x00, 0xF0, 0x41, 0x7C, 0x51, 0xF8, 0x00, 0x00, 0x50, 0x7C, 0x51, 0x01, 0x00, 0xFD, 0x00,
+    0xFC, 0x01, 0x83, 0x51,
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame7[] = {
+    4,
+    OAM_ENTRY(-12, -42, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame8[] = {
+    4,
+    OAM_ENTRY(-12, -43, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame9[] = {
+    4,
+    OAM_ENTRY(-12, -46, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sEscapeSwitchWaitingOam_Frame10[] = {
+    4,
+    OAM_ENTRY(-12, -48, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u8 sSwitchRawData_83B5858[] = {
+    0x04, 0x00, 0xF0, 0x40, 0xEC, 0x81, 0x63, 0x51, 0xC9, 0x40, 0xF4, 0x81, 0x39, 0x51, 0xD7, 0x40,
+    0xEC, 0x81, 0x1C, 0x51, 0xE7, 0x40, 0xEC, 0x41, 0x5C, 0x51,
+};
+
+const u16 sSwitchEffectActiveOam_Frame1[] = {
+    7,
+    OAM_ENTRY(-17, -32, SPRITE_SIZE_32x16, 0, 204, 6, 0),
+    OAM_ENTRY(-17, -16, SPRITE_SIZE_32x16, 0, 208, 6, 0),
+    OAM_ENTRY(-8, -45, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -49, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -41, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -25, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -16, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame2[] = {
+    8,
+    OAM_ENTRY(-12, -29, SPRITE_SIZE_16x16, 0, 212, 6, 0),
+    OAM_ENTRY(4, -29, SPRITE_SIZE_8x16, 0, 214, 6, 0),
+    OAM_ENTRY(-20, -13, SPRITE_SIZE_32x8, 0, 154, 6, 0),
+    OAM_ENTRY(-8, -43, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -47, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -39, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -23, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -14, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame3[] = {
+    6,
+    OAM_ENTRY(-10, -24, SPRITE_SIZE_16x16, 0, 215, 6, 0),
+    OAM_ENTRY(-8, -41, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -45, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -37, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -21, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-20, -14, SPRITE_SIZE_32x16, 0, 355, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame4[] = {
+    6,
+    OAM_ENTRY(-17, -29, SPRITE_SIZE_32x16, 0, 204, 6, 0),
+    OAM_ENTRY(-17, -13, SPRITE_SIZE_32x16, 0, 208, 6, 0),
+    OAM_ENTRY(-8, -39, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -43, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -35, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -19, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame5[] = {
+    7,
+    OAM_ENTRY(-12, -24, SPRITE_SIZE_16x16, 0, 212, 6, 0),
+    OAM_ENTRY(4, -24, SPRITE_SIZE_8x16, 0, 214, 6, 0),
+    OAM_ENTRY(-20, -8, SPRITE_SIZE_32x8, 0, 154, 6, 0),
+    OAM_ENTRY(-8, -37, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -41, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -33, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -17, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame6[] = {
+    6,
+    OAM_ENTRY(-13, -26, SPRITE_SIZE_16x16, 0, 215, 6, 0),
+    OAM_ENTRY(-6, -19, SPRITE_SIZE_16x16, 0, 215, 6, 0),
+    OAM_ENTRY(-8, -34, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -38, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -30, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -14, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame7[] = {
+    7,
+    OAM_ENTRY(-17, -34, SPRITE_SIZE_32x16, 0, 204, 6, 0),
+    OAM_ENTRY(-17, -18, SPRITE_SIZE_32x16, 0, 208, 6, 0),
+    OAM_ENTRY(-8, -31, SPRITE_SIZE_16x8, 0, 510, 5, 0),
+    OAM_ENTRY(-12, -35, SPRITE_SIZE_32x16, 0, 313, 5, 0),
+    OAM_ENTRY(-20, -27, SPRITE_SIZE_32x16, 0, 284, 5, 0),
+    OAM_ENTRY(-20, -11, SPRITE_SIZE_32x8, 0, 348, 5, 0),
+    OAM_ENTRY(-10, -46, SPRITE_SIZE_16x16, ST_OAM_HFLIP, 152, 4, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame8[] = {
+    4,
+    OAM_ENTRY(-12, -24, SPRITE_SIZE_16x16, 0, 212, 6, 0),
+    OAM_ENTRY(4, -24, SPRITE_SIZE_8x16, 0, 214, 6, 0),
+    OAM_ENTRY(-20, -8, SPRITE_SIZE_32x8, 0, 154, 6, 0),
+    OAM_ENTRY(-5, -44, SPRITE_SIZE_16x16, 0, 152, 4, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame9[] = {
+    3,
+    OAM_ENTRY(-9, -20, SPRITE_SIZE_16x16, 0, 215, 6, 0),
+    OAM_ENTRY(-8, -37, SPRITE_SIZE_8x8, 0, 186, 4, 0),
+    OAM_ENTRY(3, -41, SPRITE_SIZE_8x8, 0, 186, 4, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame10[] = {
+    3,
+    OAM_ENTRY(-4, -14, SPRITE_SIZE_8x8, 0, 217, 4, 0),
+    OAM_ENTRY(7, -46, SPRITE_SIZE_8x16, ST_OAM_VFLIP, 153, 4, 0),
+    OAM_ENTRY(-8, -44, SPRITE_SIZE_8x16, ST_OAM_HFLIP, 153, 4, 0),
+};
+
+const u16 sSwitchEffectActiveOam_Frame11[] = {
+    2,
+    OAM_ENTRY(-6, -40, SPRITE_SIZE_8x8, 0, 153, 4, 0),
+    OAM_ENTRY(7, -45, SPRITE_SIZE_8x8, 0, 153, 4, 0),
+};
+
+const u8 sSwitchRawData_83B59EA[] = {
+    0x00, 0x00,
+};
+
+const struct AnimationFrame sSwitchIdleOam[] = {
+    {sSwitchIdleOam_Frame1, 200},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sEscapeSwitchPressedOam[] = {
+    {sSwitchIdleOam_Frame1, 1},
+    {sEscapeSwitchPressedOam_Frame1, 4},
+    {sEscapeSwitchPressedOam_Frame2, 4},
+    {sEscapeSwitchPressedOam_Frame3, 4},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sEscapeSwitchActiveOam[] = {
+    {sEscapeSwitchPressedOam_Frame3, 200},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sSwitchReleasedOam[] = {
+    {sEscapeSwitchPressedOam_Frame3, 1},
+    {sSwitchReleasedOam_Frame1, 4},
+    {sSwitchReleasedOam_Frame2, 4},
+    {sSwitchReleasedOam_Frame3, 4},
+    {sSwitchReleasedOam_Frame4, 4},
+    {sSwitchReleasedOam_Frame5, 4},
+    {sSwitchReleasedOam_Frame6, 4},
+    {sSwitchReleasedOam_Frame7, 4},
+    {sSwitchReleasedOam_Frame8, 4},
+    {sSwitchReleasedOam_Frame9, 1},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sSwitchPressedOam[] = {
+    {sSwitchPressedOam_Frame1, 7},
+    {sSwitchPressedOam_Frame2, 7},
+    {sSwitchPressedOam_Frame3, 7},
+    {sSwitchPressedOam_Frame4, 7},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sSwitchEffectIdleOam[] = {
+    {sSwitchEffectIdleOam_Frame1, 5},
+    {sSwitchEffectIdleOam_Frame2, 5},
+    {sSwitchEffectIdleOam_Frame3, 5},
+    {sSwitchEffectIdleOam_Frame4, 5},
+    {sSwitchEffectIdleOam_Frame5, 5},
+    {sSwitchEffectIdleOam_Frame6, 5},
+    {sSwitchEffectIdleOam_Frame7, 5},
+    {sSwitchEffectIdleOam_Frame8, 5},
+    ANIMATION_TERMINATOR
+};
+
+const u8 sSwitchRawData_83B5AFC[] = {
+    0xCC, 0x57, 0x3B, 0x08, 0x05, 0x00, 0x00, 0x00, 0xDA, 0x57, 0x3B, 0x08, 0x05, 0x00, 0x00, 0x00,
+    0xE8, 0x57, 0x3B, 0x08, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+const struct AnimationFrame sEscapeSwitchWaitingOam[] = {
+    {sEscapeSwitchWaitingOam_Frame1, 19},
+    {sEscapeSwitchWaitingOam_Frame2, 12},
+    {sEscapeSwitchWaitingOam_Frame3, 12},
+    {sEscapeSwitchWaitingOam_Frame4, 12},
+    {sEscapeSwitchWaitingOam_Frame5, 12},
+    {sEscapeSwitchWaitingOam_Frame6, 12},
+    {sSwitchReleasedOam_Frame2, 12},
+    {sSwitchReleasedOam_Frame1, 12},
+    {sEscapeSwitchPressedOam_Frame3, 15},
+    {sEscapeSwitchWaitingOam_Frame7, 2},
+    {sEscapeSwitchWaitingOam_Frame8, 3},
+    {sEscapeSwitchWaitingOam_Frame9, 3},
+    {sEscapeSwitchWaitingOam_Frame10, 3},
+    {sSwitchIdleOam_Frame1, 1},
+    ANIMATION_TERMINATOR
+};
+
+const u8 sSwitchRawData_83B5B94[] = {
+    0xDA, 0x54, 0x3B, 0x08, 0x01, 0x00, 0x00, 0x00, 0x2E, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00,
+    0x48, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00, 0x62, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00,
+    0x7C, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00, 0x96, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00,
+    0xB0, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00,
+    0xE4, 0x55, 0x3B, 0x08, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+const struct AnimationFrame sEscapeSwitchIdleOam[] = {
+    {sEscapeSwitchIdleOam_Frame1, 200},
+    ANIMATION_TERMINATOR
+};
+
+const struct AnimationFrame sSwitchEffectActiveOam[] = {
+    {sEscapeSwitchIdleOam_Frame1, 1},
+    {sSwitchEffectActiveOam_Frame1, 3},
+    {sSwitchEffectActiveOam_Frame2, 3},
+    {sSwitchEffectActiveOam_Frame3, 3},
+    {sSwitchEffectActiveOam_Frame4, 3},
+    {sSwitchEffectActiveOam_Frame5, 3},
+    {sSwitchEffectActiveOam_Frame6, 3},
+    {sSwitchEffectActiveOam_Frame7, 3},
+    {sSwitchEffectActiveOam_Frame8, 3},
+    {sSwitchEffectActiveOam_Frame9, 3},
+    {sSwitchEffectActiveOam_Frame10, 3},
+    {sSwitchEffectActiveOam_Frame11, 3},
+    ANIMATION_TERMINATOR
+};
+
 void SwitchSetCommonProperties(void)
 {
     register struct PrimarySpriteData* sprite asm("ip");
